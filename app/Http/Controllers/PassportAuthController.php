dff6a5cff6829cc89f44e2bb0c6b6339f7ec8521
triest <triest21@gmail.com>
2023-03-23 12:23:07 +0300

    First commit

---
+++ app/Http/Controllers/PassportAuthController.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class PassportAuthController extends Controller
+{
+    /**
+     * Registration
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('LaravelAuthApp')->accessToken;
+
+
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('LaravelAuthApp')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+            return response($response, 422);
+        }
+    }
+}
